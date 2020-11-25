9488c288916b202ae222f6b82e78c39a82cf400b
Salvador Guzman <guzmansalv@gmail.com>
2020-11-25 13:11:02 -0800

    set db and schema

--- schema/channel.sql
+++ schema/channel.sql
@@ -1,16 +1,16 @@
-create table channels
+create table youtube.public.channels
 (
 	id bigserial not null,
 	serial char(25) not null
 );
 
 create unique index channels_id_uindex
-	on channels (id);
+	on youtube.public.channels (id);
 
 create unique index channels_serial_uindex
-	on channels (serial);
+	on youtube.public.channels (serial);
 
-alter table channels
+alter table youtube.public.channels
 	add constraint channels_pk
 		primary key (id);
 
